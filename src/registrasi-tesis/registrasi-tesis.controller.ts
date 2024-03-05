f4a74eb7888dcffe06abe3fb91c5e17ce18153a9
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-03-06 04:04:33 +0700

    feat: get registrasi tesis endpoints

--- src/registrasi-tesis/registrasi-tesis.controller.ts
+++ src/registrasi-tesis/registrasi-tesis.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post, Get, Param } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  NotFoundException,
+  UnauthorizedException,
+  Query,
+} from "@nestjs/common";
 import { RegistrasiTesisService } from "./registrasi-tesis.service";
 import { RegistrasiTopikDto } from "src/dto/registrasi-topik";
+import { RegStatus } from "src/entities/pengajuanPengambilanTopik.entity";
 
 @Controller("registrasi-tesis")
 export class RegistrasiTesisController {
@@ -23,5 +33,45 @@ export class RegistrasiTesisController {
       "test_id", // TODO: Get user id from request
       topicRegistrationDto,
     );
+  }
+
+  @Get()
+  findAll(
+    @Query()
+    query: {
+      search?: string;
+      status: RegStatus;
+      page: number;
+      limit: number;
+    },
+  ) {
+    // TODO: get id from session
+    const idPembimbing = "5f8869cf-fff0-4c71-ada2-83ddf5d8277d";
+
+    return this.registrasiTesisService.findAllRegByDosbim(
+      query.status,
+      query.page,
+      query.limit,
+      idPembimbing,
+      query.search,
+    );
+  }
+
+  @Get("/:id")
+  async findById(@Param() params: { id: string }) {
+    const res = await this.registrasiTesisService.findRegById(params.id);
+    if (!res) {
+      throw new NotFoundException("Registration not found.");
+    }
+
+    // TODO: get id from session
+    const idPembimbing = "5f8869cf-fff0-4c71-ada2-83ddf5d8277d";
+    if (res.pembimbing.id !== idPembimbing) {
+      throw new UnauthorizedException(
+        "You are not authorized to access this registration.",
+      );
+    }
+
+    return res;
   }
 }
--- src/registrasi-tesis/registrasi-tesis.service.ts
+++ src/registrasi-tesis/registrasi-tesis.service.ts
@@ -2,8 +2,11 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ModuleRef } from "@nestjs/core";
 import { getEntityManagerToken } from "@nestjs/typeorm";
-import { Repository, EntityManager } from "typeorm";
-import { PengajuanPengambilanTopik } from "src/entities/pengajuanPengambilanTopik.entity";
+import { Repository, EntityManager, Like } from "typeorm";
+import {
+  PengajuanPengambilanTopik,
+  RegStatus,
+} from "src/entities/pengajuanPengambilanTopik.entity";
 import { RegistrasiTopikDto } from "src/dto/registrasi-topik";
 import { Pengguna } from "src/entities/pengguna.entity";
 import { DosenBimbingan } from "src/entities/dosenBimbingan.entity";
@@ -77,5 +80,55 @@ export class RegistrasiTesisService {
         mahasiswaId,
       })
       .getMany();
+  }
+
+  async findAllRegByDosbim(
+    status: RegStatus,
+    page: number,
+    limit: number,
+    idPembimbing: string,
+    search?: string,
+  ) {
+    return await this.pengajuanPengambilanTopikRepository.find({
+      select: {
+        id: true,
+        waktuPengiriman: true,
+      },
+      relations: {
+        mahasiswa: true,
+      },
+      where: {
+        idPembimbing,
+        status,
+        mahasiswa: {
+          nama: Like(`%${search || ""}%`),
+        },
+      },
+      order: {
+        waktuPengiriman: "DESC",
+      },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+  }
+
+  async findRegById(id: string) {
+    return await this.pengajuanPengambilanTopikRepository.findOne({
+      select: {
+        id: true,
+        waktuPengiriman: true,
+        jadwalInterview: true,
+        waktuKeputusan: true,
+        status: true,
+      },
+      where: {
+        id,
+      },
+      relations: {
+        pembimbing: true,
+        topik: true,
+        mahasiswa: true,
+      },
+    });
   }
 }
