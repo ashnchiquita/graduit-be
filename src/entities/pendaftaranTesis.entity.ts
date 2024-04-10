a3280888d9ce8c619fe0ee922d775b9c300f5455
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-04-10 09:31:47 +0700

    feat: entities api docs

--- src/entities/pendaftaranTesis.entity.ts
+++ src/entities/pendaftaranTesis.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Pengguna } from "./pengguna.entity";
 import { Topik } from "./topik.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export enum RegStatus {
   NOT_ASSIGNED = "NOT_ASSIGNED",
@@ -23,21 +24,27 @@ export enum JalurEnum {
 
 @Entity()
 export class PendaftaranTesis {
+  @ApiProperty({ example: "550e8400-e29b-41d4-a716-446655440000" })
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @ApiProperty({ enum: JalurEnum })
   @Column({ type: "enum", enum: JalurEnum })
   jalurPilihan: JalurEnum;
 
+  @ApiProperty()
   @Column({ type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
   waktuPengiriman: Date;
 
+  @ApiProperty()
   @Column({ type: "timestamptz", nullable: true })
   jadwalInterview: Date;
 
+  @ApiProperty()
   @Column({ type: "timestamptz", nullable: true })
   waktuKeputusan: Date;
 
+  @ApiProperty({ enum: RegStatus })
   @Column({ type: "enum", enum: RegStatus, default: RegStatus.NOT_ASSIGNED })
   status: RegStatus;
 
