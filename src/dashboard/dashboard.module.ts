262a2056bc6fbe25477b124353dfefffbb32bd07
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2024-05-05 23:15:53 +0700

    feat: check based on sidsem status

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,14 +1,21 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { DashboardController } from "./dashboard.controller";
-import { DashboardService } from "./dashboard.service";
+import { BimbinganModule } from "src/bimbingan/bimbingan.module";
+import { DosenBimbingan } from "src/entities/dosenBimbingan.entity";
+import { PendaftaranSidsem } from "src/entities/pendaftaranSidsem";
 import { PendaftaranTesis } from "../entities/pendaftaranTesis.entity";
 import { Pengguna } from "../entities/pengguna.entity";
-import { BimbinganModule } from "src/bimbingan/bimbingan.module";
+import { DashboardController } from "./dashboard.controller";
+import { DashboardService } from "./dashboard.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PendaftaranTesis, Pengguna]),
+    TypeOrmModule.forFeature([
+      PendaftaranTesis,
+      Pengguna,
+      PendaftaranSidsem,
+      DosenBimbingan,
+    ]),
     BimbinganModule,
   ],
   controllers: [DashboardController],
